6433e13123fca7e31b1a0a6acb6ae62674fc45b3
Karen Masulis <kmasulis@netrixllc.com>
2022-10-14 14:36:06 -0400

    Test

---
+++ SalesLT/Views/vwBarb_S5.sql
@@ -0,0 +1,10 @@
+-- Write your own SQL object definition here, and it'll be included in your package.
+-- Write your own SQL object definition here, and it'll be included in your package.
+CREATE VIEW [SalesLT].[vwBarb_S5]
+ 
+AS
+-- Comment for TESTING
+SELECT TOP 1 *
+FROM SalesLT.Product
+
+GO
